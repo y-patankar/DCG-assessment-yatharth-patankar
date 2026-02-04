78a12e454e47033d6c6ca242c7126646052fea69
Yathartha Patankar <yatharthsp@gmail.com>
2026-02-04 01:40:07 -0800

    Assignment 1

---
+++ rush-1-5/rush.py
@@ -0,0 +1,34 @@
+import sys
+
+def rush(x,y):
+    """
+    Display a square pattern based on x (width) and y (height)
+    Args:
+    x (int): Width of the square
+    y (int): Height of the square
+    """
+    if x <= 0 or y <= 0:
+        print("Invalid size", file=sys.stderr)
+        return
+    # Your implementation here
+    if y==1:
+        print("B" * x)
+        return
+    for row in range(y):
+        if row==0 :
+            if x == 1:
+                print("B")
+            else:
+                print("A" + "B" * (x - 2) + "C")
+        elif row == y-1:
+            if x==1:
+                print("B")
+            else:
+                print("C" + "B" * (x - 2) + "A")
+
+        else:
+            if x == 1:
+                print("B")
+            else:
+                print("B" + " " * (x - 2) + "B")
+                
